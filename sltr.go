44e37122a2756807786fb2b50dedb6f38d483ff6
Conrad Constantine <conrad@1211.net>
2021-03-04 10:12:02 -0800

    sltr.go
    first version

---
+++ sltr.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+  "fmt"
+  )
+  
+func main() {
+  fmt.Printf("SLTR does not exist, and is proof that the position you are being pitched, is being offered by people who don't understand what they actually require in a GO developer\n")
+}
